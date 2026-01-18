bdb4d68b2bf56b83155f8b0fc85ca2243acf3910
Ayaan Amjad <ayaanquasmi2@eng.rizvi.edu.in>
2026-01-19 00:15:06 +0530

    Debugged and fixing: 2

--- client/metro.config.js
+++ client/metro.config.js
@@ -3,4 +3,10 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Exclude web platform to avoid mapbox-gl CSS import issues
+config.resolver = {
+  ...config.resolver,
+  platforms: ['ios', 'android'],
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
