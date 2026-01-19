6cbe150c53364185ea64bb838c9122b2c13744c9
Ayaan Amjad <ayaanquasmi2@eng.rizvi.edu.in>
2026-01-19 23:16:52 +0530

    Debugging map issue fixing with onload Error issue

--- client/src/app/_layout.tsx
+++ client/src/app/_layout.tsx
@@ -20,7 +20,7 @@ import * as Updates from 'expo-updates';
 import { useRouter } from 'expo-router';
 import { setupNotificationListener } from '../utils/notifications';
 import { initializeNotificationChannels, setupNotifeeEventHandler } from '../services/notifeeService';
-import MapboxGL from '@rnmapbox/maps';
+
 
 import '../localization/i18n';
 
@@ -59,12 +59,7 @@ function AppContent() {
   const notificationListener = useRef<Notifications.Subscription>();
 
   // Initialize Mapbox for Android - disable telemetry for new architecture
-  useEffect(() => {
-    if (Platform.OS === 'android') {
-      MapboxGL.setTelemetryEnabled(false);
-      console.log('Mapbox telemetry disabled for Android');
-    }
-  }, []);
+
 
   // OTA Updates check
   const checkForOTAUpdate = useCallback(async () => {
